b0cad95c30451851384e5fb854351ab88665123c
Tony Toscano <tonymichael16@gmail.com>
2014-09-30 17:13:55 -0400

    Reflects changes and addition's of Anand's methods as of Sept. 30

--- src/project1/BigNumber.java
+++ src/project1/BigNumber.java
@@ -9,7 +9,7 @@ import java.util.*;
  */
 public class BigNumber {
 	
-	ArrayList <String> digits;
+	ArrayList <Integer> digits;
 
 	/**
 	 * Constructor of the BigNumber class
@@ -17,13 +17,14 @@ public class BigNumber {
 	 */
 	public BigNumber(String number)
 	{
-		digits = new ArrayList<String>();
+		digits = new ArrayList<Integer>(number.length());
 		checkNumber(number);
 		if(number.matches("[0-9]+")){
 			for(int i = 0; i < number.length(); i++) {
 				char c = number.charAt(i);
-				String charToString = Character.toString(c);
-				digits.add(charToString);
+				String s = Character.toString(c);
+				Integer digit = Integer.parseInt(s);
+				digits.add(digit);
 			}
 
 		}
@@ -31,32 +32,60 @@ public class BigNumber {
 	}
 	
 	/**
-	 * Add together two BigNumbers
-	 * @param two	the second BigNumber to add to this
-	 * @return the sum of both BigNumbers
-	 */
-	public String add(BigNumber two)
-	{
-		
-		StringBuilder buf = new StringBuilder();
-		int carry = 0;
-		for(int i = digits.size() - 1,  j = two.digits.size() - 1; (i>=0 && j>=0) || carry != 0; i--,j--)
-		{
-				int digit1 = i < 0 ? 0 : Integer.parseInt((digits.get(i)));
-				int digit2 = j < 0 ? 0 : Integer.parseInt((two.digits.get(j)));
-				int digit = digit1 + digit2 + carry;
-				
-				if(digit > 9) {
-					carry = 1;
-					digit -= 10;
-				} else {
-					carry = 0;
-				}
-				buf.append(digit);
-		}
-		return buf.reverse().toString();
-	}
-	
+     * This method will add two BigNumbers
+     * @param two    the second BigNumber
+     * @return    the sum of the two BigNumbers
+     */
+    public BigNumber add(BigNumber two)
+    {
+
+        while(this.digits.size() > two.digits.size())
+        {
+            checkNumber(two);
+        }
+
+        while(this.digits.size() < two.digits.size())
+        {
+            checkNumber(this);
+        }
+
+        int carry = 0;
+        BigNumber sum = new BigNumber("");
+        String s = "";
+        for(int i = digits.size() - 1,  j = two.digits.size() - 1; (i>=0 && j>=0) || carry != 0; i--,j--)
+        {
+                int digit1 = i < 0 ? 0 : (digits.get(i));
+                int digit2 = j < 0 ? 0 : (two.digits.get(j));
+                int digit = digit1 + digit2 + carry;
+
+                if(digit > 9) {
+                    carry = 1;
+                    digit -= 10;
+                } else {
+                    carry = 0;
+                }
+                s += String.valueOf(digit);
+        }
+        for(int i = s.length() - 1; i>=0;i--)
+        {
+            int rightSideUp = Integer.parseInt(Character.toString(s.charAt(i)));
+            sum.digits.add(rightSideUp);
+        }
+       return sum;
+    }
+
+    private void checkNumber(BigNumber bigNumber) {
+        int highOrderDigit = bigNumber.digits.get(0);
+        if(highOrderDigit > 4)
+        {
+            bigNumber.digits.add(0, 9);
+        }
+
+        else {
+            bigNumber.digits.add(0, 0);
+        }
+
+    } 
 	/**
 	 * Subtract both BigNumbers
 	 * 
@@ -65,7 +94,7 @@ public class BigNumber {
 	 */
 	public String subtract(BigNumber two)
 	{
-//		two.negate();
+		two.negate();
 		return this.add(two);
 	}
 	
@@ -85,20 +114,93 @@ public class BigNumber {
 		
 	}
 	
-	/**
-	 * Helper method to check for number length of all 
-	 * @param bn1
-	 * @param bn2
-	 */
-	private void checkNumberLength(BigNumber bn1, BigNumber bn2)
-	{
-	
-	}
-	
-//	public void negate()
-//	{
-//		
-//	}
-	
-	
+	/**@Override
+     * toString method 
+     */
+    public String toString() {
+        String s = "";
+        for (String digit : digits) {
+            s = s + "" + digit;
+        }
+        //System.out.println(digit);}
+        return s;
+    }
+    
+    /**
+     * Calls toString on both of the BigNumbers and compares to see if they are
+     * equal
+     * @param b
+     * @returns 
+     */
+    public boolean equals(BigNumber b) {
+        if (toString().equals(b.toString())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * 
+     * @param y
+     * @returns:
+     * 0 if the BigNumbers are the same
+     * -1 if the passed in number is smaller
+     * 1 if the passed in number is larger
+     * 2 if there is an error
+     */
+    int compareTo(BigNumber y) {
+        if (equals(y)) {
+            return 0;
+        }
+        if (digits.size() > y.digits.size()) {
+            return 1;
+        }
+        if (digits.size() < y.digits.size()) {
+            return -1;
+        }
+        if (digits.size() == y.digits.size()) {
+            for (int i = 0; i < digits.size(); i++) {
+                if (digits.get(i).compareTo(y.digits.get(i)) == 0); else {
+                    return digits.get(i).compareTo(y.digits.get(i));
+                }
+            }
+        }
+        return 2;
+    }
+    
+ 
+    /**
+     * negates the BigNumber
+     */
+    public void negate() {
+        boolean doneZeros = false;
+        boolean doneFirst = false;
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (Integer.parseInt(digits.get(i)) > 0) {
+                doneZeros = true;
+            }
+            if (!doneFirst && doneZeros) {
+                int temp = 10 - Integer.parseInt(digits.get(i));
+                digits.set(i, "" + temp);
+                //System.out.println("made it");
+                doneFirst = true;
+            } else if (doneFirst && doneZeros) {
+                int temp = 9 - Integer.parseInt(digits.get(i));
+                digits.set(i, "" + temp);
+            }
+        }
+    }
+    
+    /**
+     * Removes extra zeroes from the front of BigNumbers
+     */
+    public void normalize() {
+        
+        for (int j = 0; j <= digits.size() + 1; j++) {
+            if (Integer.parseInt(digits.get(0)) == 0) {
+                digits.remove(0);
+            }
+        }
+    }
 }
